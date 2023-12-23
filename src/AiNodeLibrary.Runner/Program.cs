00e66314dde35da2b948e6767031bb8f331cf454
ArchLeaders <archleadership28@gmail.com>
2023-12-23 00:54:25 -0800

    Update debug runner

--- src/AiNodeLibrary.Runner/Program.cs
+++ src/AiNodeLibrary.Runner/Program.cs
@@ -10,7 +10,7 @@ Console.WriteLine(
     $"Magic: {Encoding.UTF8.GetString(BitConverter.GetBytes(ainb.Header.Magic))}"
 );
 
-Console.WriteLine($"Version: {ainb.Header.Version}");
+Console.WriteLine($"Version: {ainb.Header.Version:x2}");
 Console.WriteLine($"File Name Offset: {ainb.Header.FileNameOffset}");
 Console.WriteLine($"Command Count: {ainb.Header.CommandCount}");
 Console.WriteLine($"Node Count: {ainb.Header.NodeCount}");
@@ -26,7 +26,7 @@ Console.WriteLine($"Input/Ouput Parameters Offset: {ainb.Header.IOParametersOffs
 Console.WriteLine($"Multi Parameters Array Offset: {ainb.Header.MultiParametersArrayOffset}");
 Console.WriteLine($"Attachment Parameters Offset: {ainb.Header.AttachmentParametersOffset}");
 Console.WriteLine($"Attachment Parameters Index Array Offset: {ainb.Header.AttachmentParametersIndexArrayOffset}");
-Console.WriteLine($"Expression Binary Section Offset: {ainb.Header.ExpressionBinarySectionOffset}");
+Console.WriteLine($"Expression Binary Section Offset: {ainb.Header.ExbSectionOffset}");
 Console.WriteLine($"Child Replacment Table Offset: {ainb.Header.ChildReplacmentTableOffset}");
 Console.WriteLine($"Precondition Node Array Offset: {ainb.Header.PreconditionNodeArrayOffset}");
 Console.WriteLine($"Unknown (A): {ainb.Header.UnknownA}");
@@ -38,3 +38,68 @@ Console.WriteLine($"File Category: {ainb.Header.FileCategory}");
 Console.WriteLine($"Entry Strings Offset: {ainb.Header.EntryStringsOffset}");
 Console.WriteLine($"Unknown (D): {ainb.Header.UnknownD}");
 Console.WriteLine($"File Identification Hashes Offset: {ainb.Header.FileIdentificationHashesOffset}");
+
+Console.WriteLine($"""
+
+
+    Commands ({ainb.Header.CommandCount})
+    - - - - - - - - - -
+
+    """);
+
+foreach (var command in ainb.Commands) {
+    Console.WriteLine($"Command Name Offset: {command.NameOffset}");
+    Console.WriteLine($"ID: {command.Id}");
+    Console.WriteLine($"Left Index: {command.LeftNodeIndex}");
+    Console.WriteLine($"Right Index: {command.RightNodeIndex}");
+}
+
+Console.WriteLine($"""
+
+
+    Nodes ({ainb.Header.NodeCount})
+    - - - - - - - - - -
+
+    """);
+
+foreach (var node in ainb.Nodes) {
+    Console.WriteLine($"NodeType: {node.NodeType}");
+    Console.WriteLine($"NodeIndex: {node.NodeIndex}");
+    Console.WriteLine($"AttachmentParametersCount: {node.AttachmentParametersCount}");
+    Console.WriteLine($"NodeFlags: {node.NodeFlags}");
+    Console.WriteLine($"UserDefinedNodeTypeNameOffset: {node.UserDefinedNodeTypeNameOffset}");
+    Console.WriteLine($"NameHash: {node.NameHash}");
+    Console.WriteLine($"UnknownA: {node.UnknownA}");
+    Console.WriteLine($"NodeBodyOffset: {node.NodeBodyOffset}");
+    Console.WriteLine($"ExbFunctionCount: {node.ExbFunctionCount}");
+    Console.WriteLine($"ExbIOFieldSize: {node.ExbIOFieldSize}");
+    Console.WriteLine($"MultiParameterCount: {node.MultiParameterCount}");
+    Console.WriteLine($"UnknownB: {node.UnknownB}");
+    Console.WriteLine($"BaseAttachmentParameterIndex: {node.BaseAttachmentParameterIndex}");
+    Console.WriteLine($"BasePreconditionNode: {node.BasePreconditionNode}");
+    Console.WriteLine($"PreconditionNodeCount: {node.PreconditionNodeCount}");
+    Console.WriteLine($"SectionEntryOffset: {node.SectionEntryOffset}");
+    Console.WriteLine($"UnknownC: {node.UnknownC}");
+    Console.WriteLine($"ID: {node.Id}\n");
+}
+
+Console.WriteLine($"""
+
+
+    Local Blackboard
+    - - - - - - - - - -
+
+    """);
+
+Console.WriteLine("Headers:");
+foreach (var header in ainb.LocalBlackboard.Headers) {
+    Console.WriteLine($"  Count: {header.Count}");
+    Console.WriteLine($"  Index: {header.Index}");
+    Console.WriteLine($"  Offset: {header.Offset}\n");
+}
+
+Console.WriteLine("Entries:");
+foreach (var entry in ainb.LocalBlackboard.Entries) {
+    Console.WriteLine($"  Name Offset and Flags: {entry.NameOffsetAndFlags:b8}");
+    Console.WriteLine($"  Notes Offset: {entry.NotesOffset}\n");
+}
