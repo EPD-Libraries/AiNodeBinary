75595a5e8b7fa0a138c0e16563072fcfa7ec78f0
ArchLeaders <archleadership28@gmail.com>
2023-12-23 00:51:48 -0800

    Impl header reading and validation

--- src/AiNodeLibrary/ImmutableAinb.cs
+++ src/AiNodeLibrary/ImmutableAinb.cs
@@ -10,5 +10,13 @@ public ref struct ImmutableAinb
     public ImmutableAinb(ref RevrsReader reader)
     {
         Header = reader.Read<AinbHeader>();
+
+        if (Header.Magic != AinbHeader.AINB_MAGIC) {
+            throw new InvalidDataException("Invalid AINB magic!");
+        }
+
+        if (Header.Version is not 0x407 or 0x404) {
+            throw new InvalidDataException($"Unsupported version: {Header.Version:x2}");
+        }
     }
 }
