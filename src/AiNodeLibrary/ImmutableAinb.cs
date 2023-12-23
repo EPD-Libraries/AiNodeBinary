5b44a59337bde9976d5f303a14ba9812623920e8
ArchLeaders <archleadership28@gmail.com>
2023-12-22 21:24:34 -0800

    Create ImmtableAinb (reader)

---
+++ src/AiNodeLibrary/ImmutableAinb.cs
@@ -0,0 +1,14 @@
+﻿using AiNodeLibrary.Structures;
+using Revrs;
+
+namespace AiNodeLibrary;
+
+public ref struct ImmutableAinb
+{
+    public AinbHeader Header;
+
+    public ImmutableAinb(ref RevrsReader reader)
+    {
+        Header = reader.Read<AinbHeader>();
+    }
+}
