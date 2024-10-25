82fbd8ab28c9a0d240b8235cafeb4d7d6faabc2c
ArchLeaders <archleadership28@gmail.com>
2024-10-25 00:20:29 -0700

    Move legacy library code to .Legacy project (unused, for reference only)

---
+++ src/AinbLibrary.Legacy/Ainb.cs
@@ -0,0 +1,28 @@
+﻿using AinbLibrary.Common;
+using AinbLibrary.Structures;
+using Revrs;
+
+namespace AinbLibrary;
+
+public class Ainb
+{
+    public List<AiNode> Nodes { get; init; } = [];
+
+    public static Ainb FromBinary(Span<byte> data)
+    {
+        RevrsReader reader = new(data, Endianness.Little);
+        AinbView immutable = new(ref reader);
+        return FromAinbView(ref immutable);
+    }
+
+    public static Ainb FromAinbView(ref AinbView ainb)
+    {
+        Ainb result = new();
+
+        foreach (AinbNode node in ainb.Nodes) {
+            result.Nodes.Add(AiNode.FromNodeView(ainb, node));
+        }
+
+        return result;
+    }
+}
