d86c327e85484b847f153edd395232c1cfe3d970
ArchLeaders <archleadership28@gmail.com>
2024-10-25 00:25:56 -0700

    Create ainb command header

---
+++ src/AinbLibrary/IO/Structures/AinbCommandHeader.cs
@@ -0,0 +1,13 @@
+﻿namespace AinbLibrary.IO.Structures;
+
+public struct AinbCommandHeader
+{
+    public int NameOffset;
+    public Guid Id;
+    public ushort LeftNodeIndex;
+
+    /// <summary>
+    /// One greater than the corresponding node index.
+    /// </summary>
+    public ushort RightNodeIndex;
+}
