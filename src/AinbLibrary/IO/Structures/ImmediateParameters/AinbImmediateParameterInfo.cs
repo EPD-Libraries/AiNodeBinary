88a491a630e44923391412c6ba9fe813b6f07b3e
ArchLeaders <archleadership28@gmail.com>
2024-10-25 00:34:27 -0700

    Create immediate parameter struct

---
+++ src/AinbLibrary/IO/Structures/ImmediateParameters/AinbImmediateParameterInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.InteropServices;
+
+namespace AinbLibrary.IO.Structures.ImmediateParameters;
+
+[StructLayout(LayoutKind.Sequential, Pack = 4, Size = 8)]
+public struct AinbImmediateParameterInfo
+{
+    public int BaseIndex;
+    public int Count;
+}
