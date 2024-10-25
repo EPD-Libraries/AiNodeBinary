5e4fc0f275ebbc81254ce79f1c8b7a8d963ea9bc
ArchLeaders <archleadership28@gmail.com>
2024-10-25 00:25:41 -0700

    Create ainb node and node content header structs

---
+++ src/AinbLibrary/IO/Structures/AinbNodeContentHeader.cs
@@ -0,0 +1,48 @@
+﻿using AinbLibrary.IO.Structures.ImmediateParameters;
+using System.Runtime.InteropServices;
+
+namespace AinbLibrary.IO.Structures;
+
+public struct AinbNodeContentHeader
+{
+    public AinbImmediateParameterInfo IntImmediateParameterInfo;
+    public AinbImmediateParameterInfo BoolImmediateParameterInfo;
+    public AinbImmediateParameterInfo FloatImmediateParameterInfo;
+    public AinbImmediateParameterInfo StringImmediateParameterInfo;
+    public AinbImmediateParameterInfo VectorImmediateParameterInfo;
+    public AinbImmediateParameterInfo PointerImmediateParameterInfo;
+
+    public IoParameterInfo IntIoParameterInfo;
+    public IoParameterInfo BoolIoParameterInfo;
+    public IoParameterInfo FloatIoParameterInfo;
+    public IoParameterInfo StringIoParameterInfo;
+    public IoParameterInfo VectorIoParameterInfo;
+    public IoParameterInfo PointerIoParameterInfo;
+
+    public ConnectionInfo BoolOrFloatIoConnectionInfo;
+    public ConnectionInfo UnusedConnectionInfo1;
+    public ConnectionInfo StandardConnectionInfo;
+    public ConnectionInfo ResidentUpdateConnectionInfo;
+    public ConnectionInfo StringIoConnectionInfo;
+    public ConnectionInfo IntIoConnectionInfo;
+    public ConnectionInfo UnusedConnectionInfo2;
+    public ConnectionInfo UnusedConnectionInfo3;
+    public ConnectionInfo UnusedConnectionInfo4;
+    public ConnectionInfo UnusedConnectionInfo5;
+
+    [StructLayout(LayoutKind.Sequential, Pack = 4, Size = 16)]
+    public struct IoParameterInfo
+    {
+        public int InputBaseIndex;
+        public int InputCount;
+        public int OutputBaseIndex;
+        public int OutputCount;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1, Size = 2)]
+    public struct ConnectionInfo
+    {
+        public byte Count;
+        public byte BaseIndex;
+    }
+}
---
+++ src/AinbLibrary/IO/Structures/AinbNodeHeader.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.InteropServices;
+
+namespace AinbLibrary.IO.Structures;
+
+[StructLayout(LayoutKind.Sequential, Pack = 2)]
+public struct AinbNodeHeader
+{
+    public AinbNodeType NodeType;
+    public ushort NodeIndex;
+    public ushort AttachmentParameterCount;
+    public AinbNodeFlags NodeFlags;
+    public int UserDefinedNodeTypeNameOffset;
+    public uint NameHash;
+    public uint UnknownA;
+    public int NodeContentOffset;
+    public ushort ExbFunctionCount;
+    public ushort ExbIOFieldSize;
+    public ushort MultiParameterCount;
+    public ushort UnknownB;
+    public int BaseAttachmentParameterIndex;
+    public ushort BasePreconditionNode; // is this an index?
+    public ushort PreconditionNodeCount;
+
+    /// <summary>
+    /// Relative to the start of the file.
+    /// </summary>
+    public ushort SectionEntryOffset;
+    public ushort UnknownC;
+    public Guid Id;
+}
