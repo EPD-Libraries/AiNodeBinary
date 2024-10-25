625b963a40439f2bf8919a4025cea4db3fcf1865
ArchLeaders <archleadership28@gmail.com>
2024-10-25 00:40:39 -0700

    Update runner

--- src/AinbLibrary.Runner/Program.cs
+++ src/AinbLibrary.Runner/Program.cs
@@ -1,19 +1,22 @@
-﻿using AinbLibrary;
-using AinbLibrary.Yaml;
+﻿// JsonSerializerOptions options = new() {
+//     WriteIndented = true
+// };
+
+// byte[] data = File.ReadAllBytes(args[1]);
+// RevrsReader reader = new(data, Endianness.Little);
+// AinbView ainbView = new(ref reader);
+// Ainb ainb = Ainb.FromAinbView(ref ainbView);
+
+// using FileStream fs = File.Create("D:\\bin\\AINB\\output.yml");
+// ainbView.ToYaml(fs);
+
+// using FileStream fsJson = File.Create("D:\\bin\\AINB\\output.json");
+// JsonSerializer.Serialize(fsJson, ainb, options);
+
+using AinbLibrary;
+using AinbLibrary.IO;
 using Revrs;
-using System.Text.Json;
 
-JsonSerializerOptions options = new() {
-    WriteIndented = true
-};
-
-byte[] data = File.ReadAllBytes(args[1]);
+byte[] data = File.ReadAllBytes(args[2]);
 RevrsReader reader = new(data, Endianness.Little);
-AinbView ainbView = new(ref reader);
-Ainb ainb = Ainb.FromAinbView(ref ainbView);
-
-using FileStream fs = File.Create("D:\\bin\\AINB\\output.yml");
-ainbView.ToYaml(fs);
-
-using FileStream fsJson = File.Create("D:\\bin\\AINB\\output.json");
-JsonSerializer.Serialize(fsJson, ainb, options);
+AinbReader ainbReader = new(ref reader);
